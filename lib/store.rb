bc3496ac2fde7d2c76afda54c63e5632f2fb5220
Zeinaaaa <zainabalbasuny8181@gmail.com>
2022-01-06 12:58:03 -0500

    finish exercises

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
 
+  def at_least_one_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, 'must have at least one type of apparel')
+      errors.add(:womens_apparel, 'must have at least one type of apparel')
+    end
+  end
 end
